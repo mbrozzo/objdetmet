ea78a2c3de8f83a0985d7d00fba8de678ba47a6c
Mattia Brozzo <mbrozzo@gmail.com>
2022-07-07 13:01:56 +0200

    Simplified Box class

--- objdetmet/annotations.py
+++ objdetmet/annotations.py
@@ -8,24 +8,28 @@ class Shape2D:
 
     def __init__(self, *args):
         if len(args) == 2 and isinstance(args[0], numbers.Number) and isinstance(args[1], numbers.Number):
-            self.w = args[0]
-            self.h = args[1]
+            w = cast_if_different_class(args[0], float)
+            h = cast_if_different_class(args[1], float)
         elif len(args) == 1 and isinstance(args[0], Shape2D):
-            self.w = args[0].w
-            self.h = args[0].h
+            w = args[0].w
+            h = args[0].h
         elif len(args) == 1 and isinstance(args[0], (tuple, list)) and len(args[0]) == 2 and isinstance(args[0][0], numbers.Number) and isinstance(args[0][1], numbers.Number):
-            self.w = args[0][0]
-            self.h = args[0][1]
+            w = cast_if_different_class(args[0][0], float)
+            h = cast_if_different_class(args[0][1], float)
         else:
             raise TypeError('Expected Shape2D, tuple/list of two numbers or two numbers.')
+        if w < 0 or h < 0:
+            raise ValueError('Widths and heights must be non-negative numbers.')
+        self._w = w
+        self._h = h
     
     def _get_w(self):
         return self._w
     
     def _set_w(self, value):
-        value = float(value)
-        if value <= 0:
-            raise ValueError('Widths and heights must be positive numbers.')
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise ValueError('Widths and heights must be non-negative numbers.')
         self._w = value
 
     w = property(_get_w, _set_w, doc='Width')
@@ -34,9 +38,9 @@ class Shape2D:
         return self._h
     
     def _set_h(self, value):
-        value = float(value)
-        if value <= 0:
-            raise ValueError('Widths and heights must be positive numbers.')
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise ValueError('Widths and heights must be non-negative numbers.')
         self._h = value
 
     h = property(_get_h, _set_h, doc='Height')
@@ -54,17 +58,17 @@ class Point2D:
 
     def __init__(self, *args):
         if len(args) == 2 and isinstance(args[0], numbers.Number) and isinstance(args[1], numbers.Number):
-            self._x = args[0]
-            self._y = args[1]
+            self._x = cast_if_different_class(args[0], float)
+            self._y = cast_if_different_class(args[1], float)
         elif len(args) == 1 and isinstance(args[0], Point2D):
             self._x = args[0]._x
             self._y = args[0]._y
         elif len(args) == 1 and isinstance(args[0], (tuple, list)) and len(args[0]) == 2 and isinstance(args[0][0], numbers.Number) and isinstance(args[0][1], numbers.Number):
-            self._x = args[0][0]
-            self._y = args[0][1]
+            self._x = cast_if_different_class(args[0][0], float)
+            self._y = cast_if_different_class(args[0][1], float)
         elif len(args) == 0:
-            self._x = 0
-            self._y = 0
+            self._x = 0.0
+            self._y = 0.0
         else:
             raise TypeError('Expected Point2D, tuple/list of two numbers or two numbers.')
     
@@ -72,8 +76,7 @@ class Point2D:
         return self._x
     
     def _set_x(self, value):
-        value = float(value)
-        self._x = value
+        self._x = cast_if_different_class(value, float)
 
     x = property(_get_x, _set_x, doc='x coordinate')
     
@@ -81,8 +84,7 @@ class Point2D:
         return self._y
     
     def _set_y(self, value):
-        value = float(value)
-        self._y = value
+        self._y = cast_if_different_class(value, float)
 
     y = property(_get_y, _set_y, doc='y coordinate')
     
@@ -97,144 +99,169 @@ class Point2D:
 # Class for rectangular box
 class Box:
     
-    def __init__(self, p1=None, p2=None, pc=None, shape=None):
-        # p1, p2 and pc can be None, Point2D or any type that can be used to construct a new Point2D
+    def __init__(self, left=None, top=None, right=None, bottom=None):
         try:
-            p1 = p1 if p1 is None else cast_if_different_class(p1, Point2D)
-            p2 = p2 if p2 is None else cast_if_different_class(p2, Point2D)
-            pc = pc if pc is None else cast_if_different_class(pc, Point2D)
-        except TypeError as e:
-            raise TypeError('Parameters p1, p2 and pc must either be None, Point2D or any type that can be used to construct a new Point2D.')
+            if left is not None:
+                left = cast_if_different_class(left, float)
+            if top is not None:
+                top = cast_if_different_class(top, float)
+            if right is not None:
+                right = cast_if_different_class(right, float)
+            if bottom is not None:
+                bottom = cast_if_different_class(bottom, float)
+        except Exception as e:
+            raise TypeError('Parameters left, right, top and bottom must be float.')
         
-        # shape can be None, Shape2D or any type that can be used to construct a new Shape2D
-        try:
-            shape = shape if shape is None or isinstance(shape, Shape2D) else Shape2D(shape)
-        except TypeError as e:
-            raise TypeError('Parameter shape must either be None, Shape2D or any type that can be used to construct a new Shape2D.')
-       
-        # p1 and p2 defined
-        if p1 is not None and p2 is not None and pc is None and shape is None:
-            self._p1 = p1
-            self._p2 = p2
-        # p1 and shape defined
-        elif p1 is not None and shape is not None and p2 is None and pc is None:
-            self._p1 = p1
-            self._p2 = Point2D(p1.x + shape.w, p1.y + shape.h)
-        # pc and shape defined
-        elif pc is not None and shape is not None and p1 is None and p2 is None:
-            self._p1 = Point2D(pc.x - shape.w / 2, pc.y - shape.h / 2)
-            self._p2 = Point2D(pc.x + shape.w / 2, pc.y + shape.h / 2)
-        else:
-            raise ValueError('Either parameters p1 and p2 or p1 and shape or pc and shape must be defined.')
+        self._l = left
+        self._t = top
+        self._r = right
+        self._b = bottom
         
-        self._check_p1_p2()
-        self._check_shape()
+        self._check_lrtb()
     
     def __eq__(self, other):
-        self.p1 == other.p1
-        self.p2 == other.p2
+        return all([self._l == other._l, self._r == other._r, self._t == other._t, self._b == other._b])
+    
+    def __iter__(self):
+        '''Iterate through the four corners, starting from top-right and going clockwise'''
+        yield self.p1
+        yield self.p2
+        yield self.p3
+        yield self.p4
 
-    """Ensure x2 >= x1"""
-    def _check_x1_x2(self):
-        if self.x2 < self.x1:
-            self.x2, self.x1 = self.x1, self.x2
+    def _check_lr(self):
+        '''Ensure r >= l'''
+        if self._r < self._l:
+            self._l, self._r = self._r, self._l
     
-    """Ensure y2 >= y1"""
-    def _check_y1_y2(self):
-        if self.y2 < self.y1:
-            self.y2, self.y1 = self.y1, self.y2
+    def _check_tb(self):
+        '''Ensure b >= t'''
+        if self._b < self._t:
+            self._t, self._b = self._b, self._t
 
-    """Ensure x2 >= x1 and y2 >= y1"""
-    def _check_p1_p2(self):
-        self._check_x1_x2()
-        self._check_y1_y2()
+    def _check_lrtb(self):
+        '''Ensure r >= l and b >= t'''
+        self._check_lr()
+        self._check_tb()
     
-    """Ensure w > 0"""
-    def _check_w(self):
-        if self.x2 == self.x1:
-            raise ValueError('Width must be positive.')
+    @property
+    def left(self):
+        '''Left side x coordinate'''
+        return self._l
     
-    """Ensure h > 0"""
-    def _check_h(self):
-        if self.y2 == self.y1:
-            raise ValueError('Height must be positive.')
+    @property
+    def right(self):
+        '''Right side x coordinate'''
+        return self._r
     
-    """Ensure w > 0 and h > 0"""
-    def _check_shape(self):
-        self._check_w()
-        self._check_h()
+    @property
+    def top(self):
+        '''Top side y coordinate'''
+        return self._t
+    
+    @property
+    def bottom(self):
+        '''Bottom side y coordinate'''
+        return self._b
 
-    def _get_x1(self):
-        return self.p1.x
+    @property()
+    def coordinates(self):
+        '''Tuple of the four box coordinates: (left, top, right, bottom)'''
+        return (self._l, self._t, self._r, self._b)
     
-    def _set_x1(self, value):
-        self.p1.x = float(value)
-        self._check_x1_x2()
-    
-    x1 = property(_get_x1, _set_x1, doc='Top-left corner x coordinate')
+    def set_coordinates(left=None, top=None, right=None, bottom=None):
+        '''Set any of the four box coordinates'''
+        if left is not None:
+            self._l = cast_if_different_class(left, float)
+        if top is not None:
+            self._t = cast_if_different_class(top, float)
+        if right is not None:
+            self._r = cast_if_different_class(right, float)
+        if bottom is not None:
+            self._b = cast_if_different_class(bottom, float)
+        self._check_lrtb()
 
-    def _get_y1(self):
-        return self.p1.y
+    @property
+    def x1(self):
+        '''Top-left corner x coordinate'''
+        return self._l
     
-    def _set_y1(self, value):
-        self.p1.y = float(value)
-        self._check_y1_y2()
+    @property
+    def y1(self):
+        '''Top-left corner y coordinate'''
+        return self._t
     
-    y1 = property(_get_y1, _set_y1, doc='Top-left corner y coordinate')
+    @property
+    def p1(self):
+        '''Top-left corner Point2D'''
+        return Point2D(self._l, self._t)
 
-    def _get_p1(self):
-        return self._p1
+    @property
+    def x2(self):
+        '''Top-right corner x coordinate'''
+        return self._r
     
-    def _set_p1(self, value):
-        self._p1 = cast_if_different_class(value, Point2D)
+    @property
+    def y2(self):
+        '''Top-right corner y coordinate'''
+        return self._t
     
-    p1 = property(_get_p1, _set_p1, doc='Top-left corner point')
+    @property
+    def p2(self):
+        '''Top-right corner Point2D'''
+        return Point2D(self._r, self._t)
 
-    def _get_x2(self):
-        return self.p2.x
+    @property
+    def x3(self):
+        '''Bottom-right corner x coordinate'''
+        return self._r
     
-    def _set_x2(self, value):
-        self.p2.x = float(value)
-        self._check_x1_x2()
+    @property
+    def y3(self):
+        '''Bottom-right corner y coordinate'''
+        return self._b
     
-    x2 = property(_get_x2, _set_x2, doc='Bottom-right corner x coordinate')
+    @property
+    def p3(self):
+        '''Bottom-right corner Point2D'''
+        return Point2D(self._r, self._b)
 
-    def _get_y2(self):
-        return self.p2.y
+    @property
+    def x4(self):
+        '''Bottom-left corner x coordinate'''
+        return self._
     
-    def _set_y2(self, value):
-        self.p2.y = float(value)
-        self._check_y1_y2()
+    @property
+    def y4(self):
+        '''Bottom-left corner y coordinate'''
+        return self._b
     
-    y2 = property(_get_y2, _set_y2, doc='Bottom-right corner x coordinate')
-
-    def _get_p2(self):
-        return self._p2
+    @property
+    def p4(self):
+        '''Bottom-left corner Point2D'''
+        return Point2D(self._l, self._b)
     
-    def _set_p2(self, value):
-        self._p2 = cast_if_different_class(value, Point2D)
-    
-    p2 = property(_get_p2, _set_p2, doc='Bottom-right corner point')
-
     def _get_xc(self):
-        return (self.x2 + self.x2) / 2
+        return (self._l + self._r) / 2
     
     def _set_xc(self, value):
-        value = float(value)
-        self.x1 = value - self.w / 2
-        self.x2 = value + self.w / 2
+        value = cast_if_different_class(value, float)
+        w = self.w
+        self._l = value - w / 2
+        self._r = value + w / 2
     
-    xc = property(_get_xc, _set_xc, doc='Center point x coordinate')
+    xc = property(_get_xc, _set_xc, doc='Center x coordinate')
 
     def _get_yc(self):
-        return (self.y1 + self.y2) / 2
+        return (self._t + self._b) / 2
     
     def _set_yc(self, value):
-        value = float(value)
-        self.y1 = value - self.h / 2
-        self.y2 = value + self.h / 2
+        value = cast_if_different_class(value, float)
+        h = self.h
+        self._t = value - h / 2
+        self._b = value + h / 2
     
-    yc = property(_get_yc, _set_yc, doc='Center point y coordinate')
+    yc = property(_get_yc, _set_yc, doc='Center y coordinate')
 
     def _get_pc(self):
         return Point2D(self.xc, self.yc)
@@ -244,73 +271,93 @@ class Box:
         self.xc = value.x
         self.yc = value.y
     
-    pc = property(_get_pc, _set_pc, doc='Center point')
+    pc = property(_get_pc, _set_pc, doc='Center point2D')
 
-    def _get_w(self):
-        return self.x2 - self.x1
-    
-    def resize_w_center(self, value):
-        value = float(value)
-        if value <= 0:
-            raise TypeError('Expected a positive width.')
-        w_diff = value - self.w
-        self.x1 -= w_diff / 2
-        self.x2 += w_diff / 2
-    
-    def resize_w_left(self, value):
-        value = float(value)
-        if value <= 0:
-            raise TypeError('Expected a positive width.')
-        w_diff = value - self.w
-        self.x2 += w_diff
-    
-    w = property(_get_w, doc='Width')
+    @property
+    def w(self):
+        '''Width'''
+        return self._r - self._l
 
-    def _get_h(self):
-        return self.y2 - self.y1
-    
-    def resize_h_center(self, value):
-        value = float(value)
-        if value <= 0:
-            raise TypeError('Expected a positive height.')
-        h_diff = value - self.h
-        self.y1 -= h_diff / 2
-        self.y2 += h_diff / 2
-    
-    def resize_h_top(self, value):
-        value = float(value)
-        if value <= 0:
-            raise TypeError('Expected a positive width.')
-        h_diff = value - self.h
-        self.y2 += h_diff
-    
-    h = property(_get_h, doc='Height')
+    @property
+    def h(self):
+        '''Height'''
+        return self._b - self._t
 
+    @property
     def _get_shape(self):
+        '''Width and height as Shpe2D'''
         return Shape2D(self.w, self.h)
     
-    def resize_center(self, value):
-        value = cast_if_different_class(value, Shape2D)
-        self.resize_w_center(value.w)
-        self.resize_h_center(value.h)
+    def set_w_center(self, value):
+        '''Change width keeping the same center coordinate'''
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise TypeError('Expected a non-negative width.')
+        w_diff = value - self.w
+        self._l -= w_diff / 2
+        self._r += w_diff / 2
     
-    def resize_top_left(self, value):
-        value = cast_if_different_class(value, Shape2D)
-        self.resize_w_left(value.w)
-        self.resize_h_top(value.h)
+    def set_w_left(self, value):
+        '''Change width keeping the same top coordinate'''
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise TypeError('Expected a non-negative width.')
+        w_diff = value - self.w
+        self._r += w_diff
     
-    shape = property(_get_shape, doc='Shape')
+    def set_h_center(self, value):
+        '''Change height keeping the same center coordinate'''
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise TypeError('Expected a non-negative height.')
+        h_diff = value - self.h
+        self._t -= h_diff / 2
+        self._b += h_diff / 2
+    
+    def set_h_top(self, value):
+        '''Change height keeping the same top coordinate'''
+        value = cast_if_different_class(value, float)
+        if value < 0:
+            raise TypeError('Expected a non-negative height.')
+        h_diff = value - self.h
+        self._b += h_diff
+    
+    def set_shape_center(self, value):
+        '''Change shape keeping the same center coordinates'''
+        value = cast_if_different_class(value, Shape2D)
+        self.set_w_center(value.w)
+        self.set_h_center(value.h)
+    
+    def set_shape_top_left(self, value):
+        '''Change shape keeping the same top-left corner coordinates'''
+        value = cast_if_different_class(value, Shape2D)
+        self.set_w_left(value.w)
+        self.set_h_top(value.h)
     
     @property
     def ar(self):
-        """Aspect ratio"""
-        h = self.h()
-        return self.w() / h if h != 0 else float('NaN')
+        '''Aspect ratio'''
+        try:
+            return self.w / self.h
+        except ZeroDivisionError as e:
+            return float('NaN')
     
-    def to_rel(self, img_shape):
+    def normalize(self, img_shape):
+        '''Normalize coordinates with respect to img_shape (the image shape)'''
         img_shape = cast_if_different_class(img_shape, Shape2D)
-        return Box(p1=Point2D(self.x1 / img_shape.w, self.y1 / img_shape.h), p2=Point2D(self.x2 / img_shape.w, self.y2 / img_shape.h))
+        self.set_coordinates(left=self._l / img_shape.w, top=self._t / img_shape.h, right=self._r / img_shape.w, bottom=self._b / img_shape.h)
+    
+    def denormalize(self, img_shape):
+        '''Denormalize coordinates with respect to img_shape (the image shape)'''
+        img_shape = cast_if_different_class(img_shape, Shape2D)
+        self.set_coordinates(left=self._l * img_shape.w, top=self._t * img_shape.h, right=self._r * img_shape.w, bottom=self._b * img_shape.h)
+    
+    def to_normalized(self, img_shape):
+        '''Returns a new box whose coordinates are normalized with respect to img_shape (the image shape)'''
+        img_shape = cast_if_different_class(img_shape, Shape2D)
+        return Box(left=self._l / img_shape.w, top=self._t / img_shape.h, right=self._r / img_shape.w, bottom=self._b / img_shape.h)
 
-    def to_abs(self, img_shape):
+    def to_denormalized(self, img_shape):
+        '''Returns a new box whose coordinates are denormalized with respect to img_shape (the image shape)'''
         img_shape = cast_if_different_class(img_shape, Shape2D)
-        return Box(p1=Point2D(self.x1 * img_shape.w, self.y1 * img_shape.h), p2=Point2D(self.x2 * img_shape.w, self.y2 * img_shape.h))
+        return Box(left=self._l * img_shape.w, top=self._t * img_shape.h, right=self._r * img_shape.w, bottom=self._b * img_shape.h)
