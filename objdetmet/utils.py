7a597ee3034b1ad17e9fe671bc16d65351f32e9b
Mattia Brozzo <mbrozzo@gmail.com>
2022-09-15 12:45:39 +0200

    Clean slate

--- objdetmet/utils.py
+++
@@ -1,7 +0,0 @@
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
-def cast_if_different_class(obj, class_):
-    if isinstance(obj, class_):
-        return obj
-    return class_(obj)
