e9bae6ea9afcba9415e1765a9655d11ccaf9ecb2
Mattia Brozzo <mbrozzo@gmail.com>
2022-09-20 17:57:45 +0200

    Plots WIP

--- objdetmet.py
+++ objdetmet.py
@@ -1,8 +1,34 @@
 from pathlib import Path
 import argparse
-from types import new_class
 import numpy as np
 from tqdm import tqdm
+import json
+from matplotlib import pyplot as plt
+
+
+classes_to_integers = {
+    "1": 0,
+    "2.1/3": 1,
+    "2.2": 2,
+    "2.3/6.1": 3,
+    "4.1": 4,
+    "4.2": 5,
+    "4.3": 6,
+    "5.1": 7,
+    "5.2": 8,
+    "6.2": 9,
+    "7": 10,
+    "7E": 11,
+    "8": 12,
+    "9": 13,
+    "LQ": 14,
+    "MP": 15,
+}
+
+class_names = list(classes_to_integers.keys())
+class_names.sort(key=lambda x: classes_to_integers[x])
+class_names_with_bg = class_names + ["bg"]
+
 
 LOW = np.s_[..., :2]
 HIGH = np.s_[..., 2:]
@@ -26,9 +52,9 @@ def calculate_iou_matrix(bxs1, bxs2):
 def load_label(path):
     path = Path(path)
     empty = (
-        np.zeros((0, 1, np.int32)),
-        np.zeros((0, 4, np.float32)),
-        np.zeros((0, 1, np.float32)),
+        np.zeros((0), np.int32),
+        np.zeros((0, 4), np.float32),
+        np.zeros((0), np.float32),
     )
     if not path.exists():
         print(
@@ -52,7 +78,9 @@ def load_label(path):
                     cl, x, y, w, h, conf = split_line
                 else:
                     raise Exception("Too many or too few values in one line.")
-                cl = int(cl)
+                cl = int(float(cl))
+                x = float(w)
+                y = float(h)
                 w = abs(float(w))
                 h = abs(float(h))
                 l = x - w / 2
@@ -91,37 +119,50 @@ def calculate_confusion_matrix(
     n_classes,
 ):
     # Filter det by confidence threshold
-    conf_idxs = np.where(det_confidences >= conf_thresh)
+    conf_idxs = np.where(det_confidences >= conf_thresh)[0]
     det_classes = det_classes[conf_idxs]
     iou_matrix = iou_matrix[:, conf_idxs]
-    # Create matches matrix
-    # Matrix where: element i,j True <=> gt i and detection j boxes match
-    box_matches_matrix = np.logical_and(
-        np.logical_and(
-            iou_matrix > iou_thresh, iou_matrix == iou_matrix.max(0)
-        ),
-        iou_matrix == iou_matrix.max(1)[:, None],
-    )
-    box_matches_idxs = np.where(box_matches_matrix)
-    # Get classes of each couple of matched gt and box
-    gt_classes_matches = gt_classes[box_matches_idxs[0]]
-    det_classes_matches = det_classes[box_matches_idxs[1]]
-    classes_of_matches = np.stack([gt_classes_matches, det_classes_matches], 1)
-    # Construct confusion matrix
+    # Initialize confusion matrix
     cm = np.zeros((n_classes + 1, n_classes + 1), dtype="int")
-    # Update cm with matches
-    for match in classes_of_matches:
-        cm[match] += 1
-    # Update cm with false positives
-    fp_idxs = np.where(np.logical_not(box_matches_matrix.any(0)))
-    fp_classes = gt_classes[fp_idxs]
-    for cl in fp_classes:
-        cm[n_classes, cl] += 1
-    # Update cm with false negatives
-    fn_idxs = np.where(np.logical_not(box_matches_matrix.any(1)))
-    fn_classes = det_classes[fn_idxs]
-    for cl in fn_classes:
-        cm[cl, n_classes] += 1
+    if iou_matrix.shape[0] == 0 and iou_matrix.shape[1] == 0:
+        pass
+    if iou_matrix.shape[0] == 0:
+        # Only false positives
+        for cl in det_classes:
+            cm[n_classes, cl] += 1
+    elif iou_matrix.shape[1] == 0:
+        # Only false negatives
+        for cl in gt_classes:
+            cm[cl, n_classes] += 1
+    else:
+        # Create matches matrix
+        # Matrix where: element i,j True <=> gt i and detection j boxes match
+        box_matches_matrix = np.logical_and(
+            np.logical_and(
+                iou_matrix > iou_thresh, iou_matrix == iou_matrix.max(0)
+            ),
+            iou_matrix == iou_matrix.max(1)[:, None],
+        )
+        box_matches_idxs = np.where(box_matches_matrix)
+        # Get classes of each couple of matched gt and box
+        gt_classes_matches = gt_classes[box_matches_idxs[0]]
+        det_classes_matches = det_classes[box_matches_idxs[1]]
+        classes_of_matches = np.stack(
+            [gt_classes_matches, det_classes_matches], 1
+        )
+        # Update cm with matches
+        for match in classes_of_matches:
+            cm[match[0], match[1]] += 1
+        # Update cm with false positives
+        fp_idxs = np.where(np.logical_not(box_matches_matrix.any(0)))
+        fp_classes = det_classes[fp_idxs]
+        for cl in fp_classes:
+            cm[n_classes, cl] += 1
+        # Update cm with false negatives
+        fn_idxs = np.where(np.logical_not(box_matches_matrix.any(1)))
+        fn_classes = gt_classes[fn_idxs]
+        for cl in fn_classes:
+            cm[cl, n_classes] += 1
     return cm
 
 
@@ -130,10 +171,42 @@ def calculate_tp_fp_fn(confusion_matrix):
     tp = confusion_matrix.diagonal()
     fp = confusion_matrix.sum(0) - tp
     fn = confusion_matrix.sum(1) - tp
-    tp[-1] = tp[:-1].sum()
-    fp[-1] = fp[:-1].sum()
-    fn[-1] = fn[:-1].sum()
-    return tp, fp, fn
+    return tp[:-1], fp[:-1], fn[:-1]
+
+
+def round_to_base(x, base):
+    return base * round(float(x) / base)
+
+
+FORBIDDEN_CHARS = [
+    "\\",
+    "/",
+    ":",
+    "@",
+    "$",
+    "#",
+    "<",
+    ">",
+    "+",
+    "%",
+    "!",
+    "`",
+    "&",
+    "*",
+    "'",
+    '"',
+    "|",
+    "{",
+    "}",
+    "?",
+    "=",
+]
+
+
+def make_filename_safe(str_, replacer="_"):
+    for c in FORBIDDEN_CHARS:
+        str_ = str_.replace(c, replacer)
+    return str_
 
 
 def main():
@@ -147,10 +220,20 @@ def main():
         type=Path,
     )
     parser.add_argument(
+        "out_dir",
+        type=Path,
+    )
+    parser.add_argument(
         "--n-classes",
         "-n",
         type=int,
         default=16,
+    )
+    parser.add_argument(
+        "--conf-thresh",
+        "-c",
+        type=float,
+        default=0.1,
     )
     parser.add_argument(
         "--iou-thresh",
@@ -159,32 +242,46 @@ def main():
         default=0.5,
     )
     parser.add_argument(
-        "--conf-thresh",
-        "-c",
+        "--conf-step",
         type=float,
-        default=0.1,
+        default=0.01,
     )
+    # parser.add_argument(
+    #     "--iou-step",
+    #     type=float,
+    #     default=0.05,
+    # )
     args = parser.parse_args()
     ground_truths_dir: Path = args.ground_truths_dir
     detections_dir: Path = args.detections_dir
-    conf_thresh_default: float = args.conf_thresh
-    iou_thresh_default: float = args.iou_thresh
+    out_dir: Path = args.out_dir
+    conf_step: float = args.conf_step
+    conf_th_default: float = args.conf_thresh
+    iou_th_default: float = args.iou_thresh
     n_classes: int = args.n_classes
     # images_dir: Path = args.images_dir
 
-    iou_step = 0.05
-    iou_th_list = []
-    iou_th = 0.5
-    while iou_th < 1.0:
-        iou_th_list.append(iou_th)
-        iou_th += iou_step
+    iou_th_list = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    if iou_th_default not in iou_th_list:
+        iou_th_default = min(iou_th_list, key=lambda x: abs(x - iou_th_default))
+        print(
+            f"WARNING: iou_th_default rounded to closest available one: {iou_th_default}."
+        )
+    iou_th_default_idx = iou_th_list.index(iou_th_default)
 
-    conf_step = 0.01
     conf_th_list = []
-    conf_th = 0
-    while iou_th <= 1.0:
+    conf_th = 0.0
+    i = 0
+    while conf_th <= 1.0:
         conf_th_list.append(conf_th)
-        conf_th += conf_step
+        i += 1
+        conf_th = conf_step * i
+    if conf_th_default not in conf_th_list:
+        conf_th_default = min(
+            conf_th_list, key=lambda x: abs(x - conf_th_default)
+        )
+        print("WARNING: iou_th_default rounded to closest available one.")
+    conf_th_default_idx = conf_th_list.index(conf_th_default)
 
     # Define stats arrays
     CM_by_iou_and_conf = np.zeros(
@@ -192,32 +289,41 @@ def main():
         dtype="int",
     )
     TP_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="int"
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="int"
     )
     FP_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="int"
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="int"
     )
     FN_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="int"
-    )
-    F1_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="float"
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="int"
     )
     P_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="float"
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="float"
     )
     R_by_iou_conf_and_class = np.zeros(
-        (len(iou_th_list), len(conf_th_list), n_classes + 1), dtype="float"
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="float"
     )
-    AP_by_iou_and_class = np.zeros(
-        (len(iou_th_list), n_classes + 1), dtype="float"
+    F1_by_iou_conf_and_class = np.zeros(
+        (len(iou_th_list), len(conf_th_list), n_classes), dtype="float"
     )
+    macro_F1_by_iou_and_conf = np.zeros(
+        (len(iou_th_list), len(conf_th_list)), dtype="float"
+    )
+    micro_F1_by_iou_and_conf = np.zeros(
+        (len(iou_th_list), len(conf_th_list)), dtype="float"
+    )
+    weighted_F1_by_iou_and_conf = np.zeros(
+        (len(iou_th_list), len(conf_th_list)), dtype="float"
+    )
+    AP_by_iou_and_class = np.zeros((len(iou_th_list), n_classes), dtype="float")
     mAP_by_iou = np.zeros((len(iou_th_list)), dtype="float")
     mAP_05_095 = np.array(0)
 
     # Calculate confusion matrix
-    for gt_path in tqdm(list(ground_truths_dir.glob("*.txt"))):
-        detection_path = detections_dir / gt_path.stem
+    for gt_path in tqdm(
+        list(ground_truths_dir.glob("*.txt"))[:10]
+    ):  # TODO rimuovi [:10]
+        detection_path = detections_dir / gt_path.name
         if not detection_path.exists():
             print("WARNING: Detection label file not found, skipping.")
             continue
@@ -242,15 +348,30 @@ def main():
     # Calculate other stats
     for i, iou_th in enumerate(iou_th_list):
         for j, conf_th in enumerate(conf_th_list):
-            tp, fp, fn = calculate_tp_fp_fn(CM_by_iou_and_conf[i, j])
+            cm = CM_by_iou_and_conf[i, j]
+            tp, fp, fn = calculate_tp_fp_fn(cm)
             TP_by_iou_conf_and_class[i, j] = tp
             FP_by_iou_conf_and_class[i, j] = fp
             FN_by_iou_conf_and_class[i, j] = fn
-            p = tp / (tp + fp)
-            r = tp / (tp + fn)
+            p = np.nan_to_num(tp / (tp + fp))
+            r = np.nan_to_num(tp / (tp + fn))
             P_by_iou_conf_and_class[i, j] = p
             R_by_iou_conf_and_class[i, j] = r
-            F1_by_iou_conf_and_class[i, j] = 2 * p * r / (p + r)
+            f1 = np.nan_to_num(2 * p * r / (p + r))
+            F1_by_iou_conf_and_class[i, j] = f1
+            macro_F1_by_iou_and_conf[i, j] = f1.mean()
+            tp_total = tp.sum()
+            micro_F1_by_iou_and_conf[i, j] = np.nan_to_num(
+                tp_total / (tp_total + 0.5 * (fp.sum() + fn.sum()))
+            )
+            weights = cm.sum(1)[:-1]
+            weighted_F1_by_iou_and_conf[i, j] = np.nan_to_num(
+                (f1 * weights).sum() / weights.sum()
+            )
+    conf_th_best_idx = weighted_F1_by_iou_and_conf[
+        iou_th_list.index(iou_th_default)
+    ].argmax()
+    conf_th_best = conf_th_list[conf_th_best_idx]
 
     # Calculate AP
     for i, iou_th in enumerate(iou_th_list):
@@ -260,8 +381,103 @@ def main():
             np.maximum.accumulate(p_by_conf_and_class, 0) * r_by_conf_and_class
         ).sum(0)
         AP_by_iou_and_class[i] = ap
-        mAP_by_iou[i] = ap.mean(1)
+        mAP_by_iou[i] = ap.mean()
     mAP_05_095 = mAP_by_iou.mean()
+
+    metrics = {
+        f"Confusion Matrix @iou={iou_th_default},conf={conf_th_default}": CM_by_iou_and_conf[
+            iou_th_default_idx
+        ][
+            conf_th_default_idx
+        ].tolist(),
+        f"Confusion Matrix @iou={iou_th_default},conf={conf_th_best}": CM_by_iou_and_conf[
+            iou_th_default_idx
+        ][
+            conf_th_best_idx
+        ].tolist(),
+        f"Precision curves by class @iou={iou_th_default}": P_by_iou_conf_and_class[
+            iou_th_default_idx
+        ].tolist(),
+        f"Recall curves by class @iou={iou_th_default}": R_by_iou_conf_and_class[
+            iou_th_default_idx
+        ].tolist(),
+        f"F1 curves by class @iou={iou_th_default}": F1_by_iou_conf_and_class[
+            iou_th_default_idx
+        ].tolist(),
+        f"Macro F1 curve @iou={iou_th_default}": macro_F1_by_iou_and_conf[
+            iou_th_default_idx
+        ].tolist(),
+        f"Micro F1 curve @iou={iou_th_default}": micro_F1_by_iou_and_conf[
+            iou_th_default_idx
+        ].tolist(),
+        f"Weighted F1 curve @iou={iou_th_default}": weighted_F1_by_iou_and_conf[
+            iou_th_default_idx
+        ].tolist(),
+        f"Confidence Threshold @iou={iou_th_default},max(Weighted F1 curve)": float(
+            conf_th_best
+        ),
+        "mAP@0.5": float(mAP_by_iou[iou_th_list.index(0.5)]),
+        "mAP@0.75": float(mAP_by_iou[iou_th_list.index(0.75)]),
+        "mAP@[0.5:0.05:0.95]": float(mAP_05_095),
+    }
+    out_dir.mkdir(exist_ok=True, parents=True)
+    with (out_dir / "metrics.json").open("w") as out_json:
+        json.dump(metrics, out_json)
+
+    for title, data in metrics.items():
+        data = np.array(data)
+        if title.startswith("Confusion Matrix"):
+            # Confusion matrix
+            fig, ax = plt.subplots()
+            plt.xticks(list(range(len(data[0]))), class_names_with_bg)
+            plt.yticks(list(range(len(data))), class_names_with_bg)
+            ax.matshow(data, cmap="Blues")
+            for (i, j), z in np.ndenumerate(data):
+                ax.text(j, i, str(z), ha="center", va="center")
+            plt.title(title)
+            plt.xlabel("Predictions")
+            plt.gca().xaxis.set_label_position("top")
+            plt.ylabel("True labels")
+            plt.savefig(out_dir / f"{title}.png")
+        elif title.startswith("Precision curve") or title.startswith(
+            "Recall curve"
+        ):
+            # Precision and recall to confidence curves
+            plt.figure()
+            plt.xticks(list(range(len(conf_th_list))), conf_th_list)
+            for i in range(len(data[0])):
+                plt.plot(data[:, i], label=class_names[i])
+            plt.title(title)
+            plt.xlabel("Confidence")
+            plt.ylabel(title.split(maxsplit=1)[0])
+            plt.savefig(out_dir / f"{make_filename_safe(title)}.png")
+
+    plt.figure()
+    plt.xticks(list(range(len(conf_th_list))), conf_th_list)
+    plt.title("F1 scores by class and averaged")
+    plt.xlabel("Confidence")
+    plt.ylabel("F1 score")
+    for title, data in metrics.items():
+        data = np.array(data)
+        if "F1 curves" in title:
+            for i in range(len(data[0])):
+                plt.plot(data[:, i], label=class_names[i])
+        elif "F1 curve" in title:
+            label = title.split("@", maxsplit=1)[0][:-1]
+            plt.plot(data, label=label)
+    plt.savefig(out_dir / f"{make_filename_safe(title)}.png")
+
+    plt.figure()
+    plt.xticks(list(range(len(conf_th_list))), conf_th_list)
+    title = f"Precision-Recall curves by class @iou={iou_th_default}"
+    plt.title(title)
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    for i in range(len(class_names)):
+        p = P_by_iou_conf_and_class[iou_th_default_idx]
+        r = R_by_iou_conf_and_class[iou_th_default_idx]
+        plt.plot(r, p, label=class_names[i])
+    plt.savefig(out_dir / f"{make_filename_safe(title)}.png")
 
 
 if __name__ == "__main__":
